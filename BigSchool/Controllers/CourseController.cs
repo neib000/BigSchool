79d337bf8f3cef164ada519c144762613c36a9e9
zuradel <53078525+zuradel@users.noreply.github.com>
2021-07-02 19:49:32 +0700

    Initial commit

---
+++ BigSchool/Controllers/CourseController.cs
@@ -0,0 +1,53 @@
+﻿using BigSchool.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigSchool.Controllers
+{
+    public class CourseController : Controller
+    {
+       
+
+        // GET: Course
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Create()
+        {
+            BigSchoolContext db = new BigSchoolContext();
+            Course objCourse = new Course ();
+            objCourse.ListCategory = db.Categories.ToList();
+            return View(objCourse);
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Course objCourse)
+        {
+            BigSchoolContext db = new BigSchoolContext();
+
+            ModelState.Remove("LecturerId");
+            if (!ModelState.IsValid)
+            {
+                objCourse.ListCategory = db.Categories.ToList();
+                return View("Create", objCourse);
+            }
+
+            ApplicationUser user = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(System.Web.HttpContext.Current.User.Identity.GetUserId());
+            objCourse.LecturerId = user.Id;
+
+            db.Courses.Add(objCourse);
+            db.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
